bc95f5c9eca4551465d65f401ff16f490224a02b
bre-nda <brendaandeso4@gmail.com>
2022-12-14 18:08:33 +0300

    update the helper and nav-data.ts

---
+++ gift/src/app/sidenav/nav-data.ts
@@ -0,0 +1,69 @@
+import { INavbarData } from "./helper";
+
+export const navbarData: INavbarData[] = [
+    {
+      routeLink: 'home',
+      icon: 'fa fa-home',
+      label: 'Home', 
+    },
+    {
+        routeLink: 'messages',
+        icon: 'fa fa-commenting',
+        label: 'Message Settings', 
+    },
+    {
+        routeLink: 'purchases',
+        icon: 'fa-solid fa-cart-shopping',
+        label: 'Purchases', 
+    },
+    {
+        routeLink: 'wallets',
+        icon: 'fa-solid fa-wallet',
+        label: 'Wallets', 
+    },
+    {
+        routeLink: 'redemption',
+        icon: 'fa-solid fa-gift',
+        label: 'Redemption', 
+    },
+    {
+        routeLink: 'share',
+        icon: 'fa-sharp fa-solid fa-share-nodes',
+        label: 'Share Gifts', 
+    },
+    {
+        routeLink: 'balance',
+        icon: 'fa-solid fa-credit-card',
+        label: 'Balance', 
+    },
+    {
+        routeLink: 'friend',
+        icon: 'fa-solid fa-book',
+        label: 'Refer Friend', 
+    },
+    {
+        routeLink: 'contacts',
+        icon: 'fa-solid fa-address-card',
+        label: 'Contacts', 
+    },
+    {
+        routeLink: 'sessions',
+        icon: 'fa-solid fa-table',
+        label: 'Sessions', 
+    },
+    {
+        routeLink: 'settings',
+        icon: 'fa-solid fa-gear',
+        label: 'Other Settings',
+        expanded: true,
+        items: [
+            {
+                routeLink: 'settings/users',
+                icon: 'fa-solid fa-user',
+                label: 'Users',
+            },
+            
+        ] 
+    },
+    
+];
